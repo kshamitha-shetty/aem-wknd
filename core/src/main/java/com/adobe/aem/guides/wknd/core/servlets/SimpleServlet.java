2080c412643ee1185c7882c587f04c0bb751335f
Kshamitha <kshamitha@epsilon.com>
2020-06-10 14:56:13 +0530

    Checking AEM rules

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/SimpleServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/SimpleServlet.java
@@ -45,12 +45,13 @@ import java.io.IOException;
 public class SimpleServlet extends SlingSafeMethodsServlet {
 
     private static final long serialVersionUID = 1L;
+	public static final String JCR_TITLE = "jcr:title";
 
     @Override
     protected void doGet(final SlingHttpServletRequest req,
             final SlingHttpServletResponse resp) throws ServletException, IOException {
         final Resource resource = req.getResource();
         resp.setContentType("text/plain");
-        resp.getWriter().write("Title = " + resource.getValueMap().get(JcrConstants.JCR_TITLE));
+        resp.getWriter().write("Title = " + resource.getValueMap().get(JCR_TITLE));
     }
 }
